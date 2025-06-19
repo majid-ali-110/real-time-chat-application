d73e797dd9e8e24b7d0f90be2c4c66b5d7fa71fa
Muhammad Haneef <tuniomuhammadhaneef@gmail.com>
2025-06-19 23:33:18 +0500

    Create protectRoute middleware: check token and verify JWT

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,2 +1,23 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized - No Token Provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+    }
+  } catch (error) {
+    console.log("Error in protectRoute middleware: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
