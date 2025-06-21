f1e7ef49fd1e53e6754777e11a741331da4e3f65
Muhammad Haneef <tuniomuhammadhaneef@gmail.com>
2025-06-21 20:18:56 +0500

    Add checkAuth endpoint to verify user authentication state

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import cloudinary from "../lib/cloudinary.js";
-// Signup controller
+
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
@@ -15,6 +15,7 @@ export const signup = async (req, res) => {
     }
 
     const user = await User.findOne({ email });
+
     if (user) return res.status(400).json({ message: "Email already exists" });
 
     const salt = await bcrypt.genSalt(10);
@@ -27,6 +28,7 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
+      // generate jwt token here
       generateToken(newUser._id, res);
       await newUser.save();
 
@@ -45,11 +47,11 @@ export const signup = async (req, res) => {
   }
 };
 
-// Login controller
 export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
+
     if (!user) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
@@ -73,9 +75,8 @@ export const login = async (req, res) => {
   }
 };
 
-// Logout controller
 export const logout = (req, res) => {
-  try{
+  try {
     res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
@@ -83,7 +84,7 @@ export const logout = (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-// Update profile picture
+
 export const updateProfile = async (req, res) => {
   try {
     const { profilePic } = req.body;
@@ -107,3 +108,11 @@ export const updateProfile = async (req, res) => {
   }
 };
 
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
