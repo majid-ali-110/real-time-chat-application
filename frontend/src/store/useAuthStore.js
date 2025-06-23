f19059d72824c9b89524767be282cda6a2987fa7
Muhammad Haneef <tuniomuhammadhaneef@gmail.com>
2025-06-23 15:15:00 +0500

    setup Zustand store with authentication state

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+export const useAuthStore = create((set, get) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+  onlineUsers: [],
+  socket: null,
+}));
